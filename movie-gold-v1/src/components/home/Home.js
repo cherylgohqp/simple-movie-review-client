496f38e117a9fa1bafaed0b10ba7dd0698a2a6f5
Cheryl <cherylgohqp@users.noreply.github.com>
2024-09-10 16:07:01 +0800

    added hosted api server on koyeb instead of localhost

--- movie-gold-v1/src/components/home/Home.js
+++ movie-gold-v1/src/components/home/Home.js
@@ -2,8 +2,6 @@ import React from "react";
 import CarouselMovies from "../hero/CarouselMovies";
 
 const Home = (movies) => {
-    // console.log("isloading in Home", isLoading);
-    console.log("movies in home component", movies);
     return (
         <CarouselMovies movies={movies.movies} isLoading={movies.isLoading}/>
     )
