cead29cfef82acb8bdbc180ddc2272febe7140af
Cheryl <cherylgohqp@users.noreply.github.com>
2024-09-11 13:40:55 +0800

    Added disabled button state when review form is empty

--- movie-gold-v1/src/components/reviewform/ReviewForm.js
+++ movie-gold-v1/src/components/reviewform/ReviewForm.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-const ReviewForm = ({handleSubmit,revText,labelText,defaultValue}) => {
+const ReviewForm = ({handleSubmit,revText,labelText}) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
     return (
   
       <Form>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
               <Form.Label>{labelText}</Form.Label>
-              <Form.Control ref={revText} as="textarea" rows={3} defaultValue={defaultValue} />
+              <Form.Control ref={revText} as="textarea" rows={3}  value={inputValue}
+          onChange={handleInputChange} placeholder='Write your review here!' />
           </Form.Group>
-          <Button variant="outline-info" onClick={handleSubmit}>Submit</Button>
+          <Button variant="outline-info" onClick={handleSubmit} disabled={inputValue.trim() === ''}>Submit</Button>
       </Form>
   
     )
