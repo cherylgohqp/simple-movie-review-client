50927e6d1a80237ad0bcf117d9c26629bf01bb14
Cheryl <cherylgohqp@users.noreply.github.com>
2024-09-11 14:38:45 +0800

    Update ReviewForm.js

--- movie-gold-v1/src/components/reviewform/ReviewForm.js
+++ movie-gold-v1/src/components/reviewform/ReviewForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import "./ReviewForm.css";
+// import "./ReviewForm.css";
 
 const ReviewForm = ({handleSubmit,revText,labelText}) => {
   const [inputValue, setInputValue] = useState('');
